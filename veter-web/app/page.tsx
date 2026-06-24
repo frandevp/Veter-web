c585e3652fea2b7f8bc35a729df75c174fde15d6
Francodedesign <francescalombardopz@gmail.com>
2026-06-24 19:14:03 +0200

    fix: imagen testimonios más grande (460x520)

--- veter-web/app/page.tsx
+++ veter-web/app/page.tsx
@@ -172,9 +172,9 @@ Ver todos nuestros servicios
 
 {/* imagen blob */}
 <div className="flex justify-center">
-<div className="relative w-[340px] h-[380px] shrink-0">
+<div className="relative w-[460px] h-[520px] shrink-0">
 {/* mancha de color detras del blob para dar profundidad */}
-<div className="absolute -bottom-4 -right-4 w-[320px] h-[360px] opacity-20 rounded-full"
+<div className="absolute -bottom-4 -right-4 w-[440px] h-[500px] opacity-20 rounded-full"
 style={{
 backgroundColor: "#ea4f4e",
 borderRadius: "72% 28% 38% 62% / 55% 67% 33% 45%",
@@ -182,8 +182,8 @@ borderRadius: "72% 28% 38% 62% / 55% 67% 33% 45%",
 <Image
 src="/images/clientes.jpg"
 alt="Clientes satisfechos de Veter Centro Veterinario"
-width={340}
-height={380}
+width={460}
+height={520}
 className="object-cover w-full h-full"
 style={{
 borderRadius: "72% 28% 38% 62% / 55% 67% 33% 45%",
